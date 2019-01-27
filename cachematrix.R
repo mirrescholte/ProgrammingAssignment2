2df3000a783eadb868e408463ea5376a37fed394
mirrescholte <mirrescholte@gmail.com>
2019-01-27 11:03:55 +0100

    Programming assignment 2 Mirre Scholte

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+#The function makeCacheMatrix creates an environment that contains the functions inverse and x within the set, get, setsolve, getsolve environment.
+#The function cacheSolve creates a function that first checks if there is already an inverse in the makeCacheMatrix getsolve function
+# if not, it computes the setsolve function.
+
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#The function makeCacheMatrix creates an environment that contains the functions inverse and x within the set, get, setsolve, getsolve environment.
 
+makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(inverse) inverse <<- solve
+        getsolve <- function() inverse
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+#The cacheSolve function first checks if there is already an inverse available, if not, it computes the inverse within the get, set, getsolve, setsolve functions.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getsolve()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setsolve(inverse)
+        inverse
 }
